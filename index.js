3f2b95fedfded75d7c95fdb921f74a6ec1d65284
lfiore18 <lfiore18@yahoo.co.uk>
2024-08-23 11:12:10 +0100

    Basic styling applied using tailwindcss

---
+++ index.js
@@ -0,0 +1,34 @@
+function dateDiffInDays(a, b)
+{
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+  
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+function calculatePriceOfStay(e)
+{
+    // get dates 
+    let checkInDate = document.getElementById("check-in-date").value;
+    let checkOutDate = document.getElementById("check-out-date").value;
+
+    const totalPriceSpan = document.getElementById("total-price");
+    
+    // hardcoded price per day
+    const pricePerDay = 199;
+
+    // convert to date objects to work with dateDiffInDays
+    checkInDate = new Date(checkInDate);
+    checkOutDate = new Date(checkOutDate);
+ 
+    let numOfDaysToStay = dateDiffInDays(checkInDate, checkOutDate);
+
+    // Change the price per stay
+    totalPriceSpan.innerText = `£${numOfDaysToStay * pricePerDay}`;
+}
+
+
+
+//console.log(calculatePriceOfStay());
